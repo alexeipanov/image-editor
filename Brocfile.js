4a3eb73469a681638c15f47a79322bed16c287ca
Alexei Panov <a.panov@zoho.com>
2018-06-08 10:58:35 +0300

    initial

---
+++ Brocfile.js
@@ -0,0 +1,47 @@
+let packages = {
+  babel: require('broccoli-babel-transpiler'),
+  sass: require('broccoli-sass'),
+  merge: require('broccoli-merge-trees'),
+  concat: require('broccoli-concat'),
+  funnel: require('broccoli-funnel'),
+  reload: require('broccoli-livereload'),
+};
+
+let appHtml = new packages.funnel('app', { files: ['index.html'] });
+let jquery = new packages.funnel('node_modules/jquery/dist/', { files: ['jquery.min.js'], destDir: 'vendor/js' });
+let fabric = new packages.funnel('node_modules/fabric/dist/', { files: ['fabric.js'], destDir: 'vendor/js' });
+let bootstrapJs = new packages.funnel('node_modules/bootstrap/dist/js', { files: ['bootstrap.min.js'], destDir: 'vendor/js' });
+let bootstrapCss = new packages.funnel('node_modules/bootstrap/dist/css', { files: ['bootstrap.min.css'], destDir: 'vendor/css' });
+let appCss = new packages.sass(['app/scss'], 'app.scss', 'css/app.css');
+
+let appJs = new packages.babel('app/js', {
+  browserPolyfill: true,
+  minified: false,
+  presets: [
+    ['env', {
+      'targets': {
+        'browsers': [
+          'ie 11',
+          'last 1 Chrome versions',
+          'last 1 Firefox versions',
+          'last 1 Safari versions'
+        ]
+      }
+    }]
+  ],
+});
+
+appJs = new packages.concat(appJs, {
+  inputFiles: ['**/*.js'],
+  outputFile: 'app.js'
+});
+
+appJs = new packages.funnel(appJs, { destDir: 'js' });
+
+let app = new packages.merge([appJs, appCss, appHtml, jquery, fabric, bootstrapJs, bootstrapCss], { overwrite: true });
+
+app = new packages.reload(app, {
+  target: /^[a-zA-Z0-9._-]+.html$/
+});
+
+module.exports = app;
