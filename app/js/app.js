92aef519eb004b2c6f782541773f856010f4458b
Alexei Panov <a.panov@zoho.com>
2018-06-12 15:31:37 +0300

    draw text and lines

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-  var editor = new ImageEditor({ id: 'editor', width: 800, height: 600 });
+  var editor = new ImageEditor({ id: 'editor', width: 800, height: 600, opacity: 0.5 });
+
+  function toggleButtonState(button, buttonClass) {
+    $('.' + buttonClass + '.active').attr('aria-pressed', false);
+    $('.' + buttonClass + '.active').removeClass('active');
+    button.attr('aria-pressed', true);
+    button.addClass('active');
+  }
+
+  $('.set-drawing-color, .set-text-color').each(function(index, item) {
+    $(item).find('span').css('color', $(item).data('color'));
+  });
 
   $('#input-image').on('change', function() {
     var file = $(this)[0].files[0];
@@ -7,5 +18,54 @@ $(document).ready(function() {
       editor.setBackground(window.URL.createObjectURL(file));
     }
   });
+
+  $('.set-drawing-color').on('click', function(event) {
+    event.preventDefault();
+    toggleButtonState($(this), 'set-drawing-color');
+    editor.setDrawingColor($(this).data('color'));
+  });
+
+  $('.set-drawing-size').on('click', function(event) {
+    event.preventDefault();
+    toggleButtonState($(this), 'set-drawing-size');
+    editor.setDrawingWidth($(this).data('size'));
+  });
+
+  $('.set-text-color').on('click', function(event) {
+    event.preventDefault();
+    toggleButtonState($(this), 'set-text-color');
+    editor.setTextColor($(this).data('color'));
+  });
+
+  $('#pencil').on('click', function(event) {
+    event.preventDefault();
+    toggleButtonState($(this), 'tool-btn');
+    editor.setDrawingMode(true);
+    editor.setPencil();
+  });
+
+  $('#brush').on('click', function(event) {
+    event.preventDefault();
+    toggleButtonState($(this), 'tool-btn');
+    editor.setDrawingMode(true);
+    editor.setBrush();
+  });
+
+  $('#text').on('click', function(event) {
+    event.preventDefault();
+    toggleButtonState($(this), 'tool-btn');
+    editor.setDrawingMode(false);
+    editor.addText('default text', { fontSize: 32 });
+  });
+
+  $('#select').on('click', function(event) {
+    event.preventDefault();
+    toggleButtonState($(this), 'tool-btn');
+    editor.setDrawingMode(false);
+  });
+
+  $('.set-drawing-color').first().trigger('click');
+  $('.set-text-color').first().trigger('click');
+  $('.set-drawing-size').first().trigger('click');
 
 });
--- app/js/editor.js
+++ app/js/editor.js
@@ -4,6 +4,8 @@ class ImageEditor {
   constructor(options) {
     this.config = options;
     this.canvas = new fabric.Canvas(this.config.id);
+    this.canvas.freeDrawingBrush = new fabric.PencilBrush(this.canvas);
+    this.canvas.freeDrawingBrush.shadow = new fabric.Shadow();
   }
 
   setBackground(image) {
@@ -17,6 +19,63 @@ class ImageEditor {
       source.set({scaleX: scale, scaleY: scale, originX: 'left', originY: 'top'});
       this.canvas.setBackgroundImage(source, this.canvas.renderAll.bind(this.canvas));
     });
+  }
+
+  setDrawingMode(mode) {
+    this.canvas.isDrawingMode = mode;
+  }
+
+  setPencil() {
+    this.canvas.freeDrawingBrush.shadow.blur = 0;
+    this.canvas.freeDrawingBrush.strokeLineCap = 'square';
+    this.canvas.freeDrawingBrush.strokeLineJoin = 'bevel';
+  }
+
+  setBrush() {
+    this.canvas.freeDrawingBrush.shadow.blur = this.canvas.freeDrawingBrush.width / 2;
+    this.canvas.freeDrawingBrush.strokeLineCap = 'round';
+    this.canvas.freeDrawingBrush.strokeLineJoin = 'round';
+  }
+
+  setDrawingColor(color) {
+    var drawingColor = new fabric.Color(color);
+    drawingColor.setAlpha(this.config.opacity);
+    this.canvas.freeDrawingBrush.color = drawingColor.toRgba();
+    this.canvas.freeDrawingBrush.shadow.color = drawingColor.toRgba();
+  }
+
+  setTextColor(color) {
+    this.textColor = new fabric.Color(color);
+    this.textColor.setAlpha(this.config.opacity);
+  }
+
+  setDrawingWidth(size) {
+    this.canvas.freeDrawingBrush.width = size;
+  }
+
+  addText(text, options) {
+    var config = {
+      fill: this.textColor.toRgba(),
+      fontSize: 12,
+      fontFamily: 'Sans Serif',
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+      textDecoration: 'none',
+      textAlign: 'left',
+      scaleX: 2,
+      scaleY: 2,
+      lineHeight: 1.15
+    };
+
+    Object.getOwnPropertyNames(config).forEach(value => {
+      config[value] = options[value] || config[value];
+    });
+
+    var itext = new fabric.IText(text, config);
+    itext.set({left: this.canvas.width / 2 - itext.width, top: this.canvas.height / 2 - itext.height});
+    this.canvas.add(itext);
+    this.canvas.setActiveObject(itext);
+    this.canvas.renderAll();
   }
 
 }
